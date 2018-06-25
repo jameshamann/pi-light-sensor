6f4c0f7ef49c5afb219c6f6f9353c782e558e691
James Hamann <jameshamann@me.com>
2018-06-25 21:08:32 +0100

    changing light sensor code

--- light_sensor.py
+++ light_sensor.py
@@ -1,7 +1,40 @@
+#!/usr/local/bin/python
+
 import RPi.GPIO as GPIO
+import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(4,GPIO.IN)
+__author__ = 'Gus (Adapted from Adafruit)'
+__license__ = "GPL"
+__maintainer__ = "pimylifeup.com"
 
-for i in range(0,5):
-    print GPIO.input(4)
+GPIO.setmode(GPIO.BOARD)
+
+#define the pin that goes to the circuit
+pin_to_circuit = 7
+
+def rc_time (pin_to_circuit):
+    count = 0
+
+    #Output on the pin for
+    GPIO.setup(pin_to_circuit, GPIO.OUT)
+    GPIO.output(pin_to_circuit, GPIO.LOW)
+    time.sleep(0.1)
+
+    #Change the pin back to input
+    GPIO.setup(pin_to_circuit, GPIO.IN)
+
+    #Count until the pin goes high
+    while (GPIO.input(pin_to_circuit) == GPIO.LOW):
+        count += 1
+
+    return count
+
+#Catch when script is interupted, cleanup correctly
+try:
+    # Main loop
+    while True:
+        print rc_time(pin_to_circuit)
+except KeyboardInterrupt:
+    pass
+finally:
+    GPIO.cleanup()
